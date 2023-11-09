b0d5a3a16f665e581df8b2711d10c7dbaf9a3f23
bgiri_gcloud <49786519+bgirigcloud@users.noreply.github.com>
2023-11-09 21:09:03 +0530

    Update scripts.sh

--- scripts.sh
+++ scripts.sh
@@ -1,6 +1,6 @@
 Warning: You must assign the Invoker role (roles/run.invoker) through Cloud Run for 2nd gen functions if you want to allow the function to receive requests from additional principals or other given authorities in IAM.
 Note: if you want to achieve the equivalent of assigning the Cloud Functions "Admin" or "Developer" role to a 1st gen function, you need to assign Cloud Functions "Admin" or "Developer" to the 2nd gen function and assign "Cloud Run Invoker" to the Cloud Run service.
-$
-gcloud functions add-invoker-policy-binding demo-mssql-auto-bkp \
-      --region="us-central1" \
-      --member="MEMBER_NAME"
+$ gcloud functions add-iam-policy-binding demo-mssql-auto-bkp \
+    --region="us-central1" \
+    --member="serviceAccount:demo-for-sql-auto-bkp@kinetic-object-400913.iam.gserviceaccount.com" \
+    --role="roles/cloudfunctions.invoker"
